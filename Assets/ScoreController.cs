67a6cdd66ed869c1e8a431539542df9dd7843403
bitbuc13 <s13silvia13@gmail.com>
2019-02-18 02:22:07 +0900

    [追加]得点表示機能を追加
    画面右上に得点が表示される
    ターゲット（大小の星と雲）にボールが衝突した時、得点が加算される
    ターゲット毎に得点の増分が異なる

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //スコア
+    public int score = 0;
+
+    // Use this for initialization
+    void Start () {
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.scoreText.GetComponent<Text>().text = "SCORE:" + this.score;
+	}
+}
